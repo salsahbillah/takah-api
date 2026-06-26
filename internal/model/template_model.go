44237e43115f96be8859dc3d0beb3749b22a3cab
nabaimelda <nabaimelda15@gmail.com>
2026-06-26 15:23:43 +0700

    add template surat

--- internal/model/template_model.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type TemplateSurat struct {
-	ID           int    `json:"id"`
-	TakahID      int    `json:"takah_id"`
-	TemplateName string `json:"template_name"`
-	Content      string `json:"content"`
-	CreatedAt    string `json:"created_at"`
-}
