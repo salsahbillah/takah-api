e305bab3d2aceecd024778e1564a76ce186decbb
cacaa19 <cacaaaa1919@gmail.com>
2026-06-29 21:03:00 +0700

    menambahkan konfigurasi CORS untuk integrasi frontend

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,18 +1,39 @@
 package main
 
 import (
+	"time"
+
 	"takah-api/internal/database"
 	"takah-api/internal/route"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-
-	// Koneksi ke database MySQL
 	database.ConnectDatabase()
 
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{
+			"http://localhost:5173",
+		},
+		AllowMethods: []string{
+			"GET",
+			"POST",
+			"PUT",
+			"DELETE",
+			"OPTIONS",
+		},
+		AllowHeaders: []string{
+			"Origin",
+			"Content-Type",
+			"Authorization",
+		},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	route.SetupRoutes(router)
 
